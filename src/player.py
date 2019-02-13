9c24773c39408fe593b26557b27c4253faab7dea
Edward J <edjeong99@gmail.com>
2019-02-13 16:10:38 -0500

    init

--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player():
+    def __init__(self, name, room):
+        self.name = name
+        self.room = room
+        self.items = []
+
+    def getItem(self, item):
+        result = list(filter(lambda x: x == item, self.room.items))
+        if len(result) > 0:
+            self.items.append(item)
--- src/room.py
+++ src/room.py
@@ -1,2 +1,20 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room():
+    def __init__(self, name,):
+        self.name = name
+        self.n_to = ''
+        self.s_to = ''
+        self.e_to = ''
+        self.w_to = ''
+        self.items = []
+
+    def addItemToRoom(self, item):
+        self.items.append(item)
+
+    def printItems():
+        printf("{self.name} has following items : \n")
+        for i in range(len(self.items)):
+            printf(self.items[i])
