7f207d95659a67e24b27b72cf6096830ead67cec
Edward J <edjeong99@gmail.com>
2019-02-13 16:43:37 -0500

    player move different room

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,6 @@
 from room import Room
+from player import Player
+from item import Item
 
 # Declare all the rooms
 
@@ -6,19 +8,13 @@ room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer", "Dim light filters in from the south. Dusty passages run north and east."),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage", "The narrow passage bends here from west to north. The smell of gold permeates the air."),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room("Treasure Chamber", "You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."),
 }
 
 
@@ -37,15 +33,28 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 
-# Make a new player object that is currently in the 'outside' room.
-p = player("tom", "outside")
+
+def p_move(p, direction):
+    direction = direction + "_to"
+    p.current_room = getattr(p.current_room, direction)
+
+
+    # Make a new player object that is currently in the 'outside' room.
+p = Player("tom", room['outside'])
 # Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+
+while True:
+    print(f"\n\nYou are in the {p.current_room.name}\n")
+    print(p.current_room.description)
+
+    pinput = input("What do you want to do next? ")
+    p_move(p, pinput)
+
+ # * Prints the current room name
+ # * Prints the current description (the textwrap module might be useful here).
+ # * Waits for user input and decides what to do.
+ #
+ # If the user enters a cardinal direction, attempt to move to the room there.
+ # Print an error message if the movement isn't allowed.
+ #
+ # If the user enters "q", quit the game.
--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name, room):
+        self.name = name
+        self.current_room = room
+        self.items = []
+
+    def add_item(self, item):
+        self.player_items.append(item)
+        self.current_room.items.remove(item)
