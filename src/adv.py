cbdf26ef142182b1f4da02d9280b749dc7d18b56
Edward J <edjeong99@gmail.com>
2019-02-13 17:03:29 -0500

    displaying items in room

--- src/adv.py
+++ src/adv.py
@@ -2,19 +2,23 @@ from room import Room
 from player import Player
 from item import Item
 
+
+# Items
+item1 = Item("item1", "it is item1")
+item2 = Item("item2", "it is item2")
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", [item1, item2]),
 
-    'foyer':    Room("Foyer", "Dim light filters in from the south. Dusty passages run north and east."),
+    'foyer':    Room("Foyer", "Dim light filters in from the south. Dusty passages run north and east.", []),
 
-    'overlook': Room("Grand Overlook", "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."),
+    'overlook': Room("Grand Overlook", "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.", []),
 
-    'narrow':   Room("Narrow Passage", "The narrow passage bends here from west to north. The smell of gold permeates the air."),
+    'narrow':   Room("Narrow Passage", "The narrow passage bends here from west to north. The smell of gold permeates the air.", []),
 
-    'treasure': Room("Treasure Chamber", "You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."),
+    'treasure': Room("Treasure Chamber", "You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.", []),
 }
 
 
@@ -48,7 +52,7 @@ p = Player("tom", room['outside'])
 while True:
     print(f"\n\nYou are in the {p.current_room.name}\n")
     print(p.current_room.description)
-
+    p.current_room.display_items()
     pinput = input("What do you want to do next? ")
 
     if pinput == 'n' or pinput == 's' or pinput == 'e' or pinput == 'w':
--- src/room.py
+++ src/room.py
@@ -3,7 +3,13 @@
 
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
-        self.items = []
+        self.items = items
+
+    def display_items(self):
+        print(f"{self.name} has {len(self.items)} items : \n")
+
+        for i in range(len(self.items)):
+            print(f"item #{i+1} : {self.items[i].name}")
